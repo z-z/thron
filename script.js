7ff528464d59cf298b1a6cb4b4037399ac3835d6
z-z <bazzaurik@gmail.com>
2015-10-08 20:16:32 +0400

    show grid

--- script.js
+++ script.js
@@ -6,27 +6,41 @@ var game = {
 	
 	
 	ctx: null,
-	cs: 10,
-	cxcount: 50,
+	cxcount: 50,  // horizontal lines count
 	cycount: 50,
 	cw: 0,
 	ch: 0,
+	xstep: 0,
+	ystep: 0,
 	
 	
 	init: function(cnv){
 		this.ctx = cnv.getContext('2d');
 		this.cw = cnv.width;
 		this.ch = cnv.height;
+		this.xstep = this.cw / this.cxcount;
+		this.ystep = this.ch / this.cycount;
 		this.render();
 	},
 	
 	render: function(){
-		this.ctx.beginPath();
 		this.ctx.fillStyle = 'green';
 		this.ctx.fillRect(0, 0, this.cw, this.ch);
-		this.ctx.fillStyle = 'red';
-		this.ctx.arc(100, 100, 10, 0, 2*Math.PI);
-		this.ctx.fill();
+		this.showGrid();
+	},
+
+	showGrid: function(){
+		this.ctx.beginPath();
+		this.ctx.strokeStyle = '#32cd32';
+		for(var i = this.xstep; i < this.cw; i += this.xstep) {
+			this.ctx.moveTo(0, i);
+			this.ctx.lineTo(this.cw, i);
+		}
+		for(i = this.ystep; i < this.ch; i += this.ystep) {
+			this.ctx.moveTo(i, 0);
+			this.ctx.lineTo(i, this.ch);
+		}
+		this.ctx.stroke();
 		this.ctx.closePath();
 	},
 };
